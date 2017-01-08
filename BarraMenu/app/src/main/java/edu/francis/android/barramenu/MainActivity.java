4eb7e4390f814e645d41edd1507d066cccedb30b
Joel Eche <joel.eche5@gmail.com>
2017-01-08 18:04:32 -0500

    Menú de Contexto

--- BarraMenu/app/src/main/java/edu/francis/android/barramenu/MainActivity.java
+++ BarraMenu/app/src/main/java/edu/francis/android/barramenu/MainActivity.java
@@ -3,8 +3,15 @@ package edu.francis.android.barramenu;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvNombre=(TextView)findViewById(R.id.tvNombre);
+        registerForContextMenu(tvNombre);
     }
 
     @Override
@@ -33,5 +43,24 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_contexto,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mEdit:
+                Toast.makeText(this,getResources().getString(R.string.menu_edit),Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mDelete:
+                Toast.makeText(this,getResources().getString(R.string.menu_delete),Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onContextItemSelected(item);
     }
 }
